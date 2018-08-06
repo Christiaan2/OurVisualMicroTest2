d8e536b01c72b64ad5d1707b2ad86699bc1ef0c9
Christiaan2 <christiaan_gsv@hotmail.com>
2018-08-06 20:54:48 +0200

    Fix errors

--- OurVisualMicroTest2.ino
+++ OurVisualMicroTest2.ino
@@ -8,16 +8,16 @@ void setup()
 	Serial.begin(115200);
 	Serial.println("Hello, this is a test");
 	pinMode(DefaultLED, OUTPUT);
-	digitalWrite(LED, LOW);
+	digitalWrite(DefaultLED, LOW);
 }
 
 void loop()
 {
-	digitalWrite(LED, LOW);
+	digitalWrite(DefaultLED, LOW);
 	delay(1000);
 	Serial.print("X = ");
 	Serial.println(x);
-	digitalWrite(LED, HIGH);
+	digitalWrite(DefaultLED, HIGH);
 	delay(1000);
 	x++;
 	if (x > 9) {
